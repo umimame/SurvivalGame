e2508474daf6f275833f886ffdd9dccc29ddc64e
22CI0XXXX <22ci0xxxx@jec.ac.jp>
2023-12-07 11:31:25 +0900

    PlayerRespawn関数化、複数種類のキャラクターには未対応（これからやります）

--- Assets/0417/Script/PlayerRespawnPos.cs
+++ Assets/0417/Script/PlayerRespawnPos.cs
@@ -17,15 +17,21 @@ public class PlayerRespawnPos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Zキーでリスポーン
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            PlayerRespawn();
+        }
+    }
+
+    void PlayerRespawn()
+    {
         //リスポーンさせる位置をランダムで設定
         int RandomRespawn = Random.Range(0, RespawnPos.Length);
         float x1 = RespawnPos[RandomRespawn].position.x;
         float y1 = RespawnPos[RandomRespawn].position.y;
         float z1 = RespawnPos[RandomRespawn].position.z;
-        //Zキーでリスポーン
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Instantiate(createPlayer[0], new Vector3(x1, y1, z1), createPlayer[0].transform.rotation);
-        }
+        //プレイヤーのインスタンスを生成     
+         Instantiate(createPlayer[0], new Vector3(x1, y1, z1), createPlayer[0].transform.rotation);
     }
 }
