16e76ca87f1614f57e262ecc8cf6d803bc4e9f42
22CI0XXXX <22ci0xxxx@jec.ac.jp>
2023-12-07 10:56:16 +0900

    playerrespawnとりあえず

--- Assets/0417/Script/PlayerRespawnPos.cs
+++ Assets/0417/Script/PlayerRespawnPos.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerRespawnPos : MonoBehaviour
 {
+    //プレイヤーObject
     [SerializeField] private GameObject[] createPlayer;
+    //リスポーン位置の設定
     [SerializeField] private Transform[] RespawnPos;
     // Start is called before the first frame update
     void Start()
@@ -15,10 +17,12 @@ public class PlayerRespawnPos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //リスポーンさせる位置をランダムで設定
         int RandomRespawn = Random.Range(0, RespawnPos.Length);
         float x1 = RespawnPos[RandomRespawn].position.x;
         float y1 = RespawnPos[RandomRespawn].position.y;
         float z1 = RespawnPos[RandomRespawn].position.z;
+        //Zキーでリスポーン
         if (Input.GetKeyDown(KeyCode.Z))
         {
             Instantiate(createPlayer[0], new Vector3(x1, y1, z1), createPlayer[0].transform.rotation);
